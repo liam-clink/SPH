622b787cfca1406bdcfd9482899b1c80db67fe58
Liam Clink <clink.6@osu.edu>
2020-02-27 16:45:53 -0500

    Added kernel header

---
+++ kernel.h
@@ -0,0 +1,12 @@
+// kernel.h
+//
+// Author: Liam Clink <clink.6@osu.edu>
+//
+// This header defines an interface for the kernel functions that
+// I use in my simulations.
+//
+
+double cubic_sph_kernel(double scaled_distance);
+double gradient_cubic_sph_kernel(double scaled_distance);
+
+
